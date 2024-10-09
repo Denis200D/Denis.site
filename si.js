58be05c20b1d9a1e0647500374f9c765e5c1e2b1
Denis200D <andreidarius642@gmail.com>
2024-10-09 10:41:20 +0300

    Add files via upload

---
+++ si.js
@@ -0,0 +1,15 @@
+let a = prompt(`how are you?`)
+if (a == `bad`) alert("why?:(")
+alert(`im  not gonna see these messages :(  `)
+confirm(`even tho im not gonna se this, have you seen the website when it didn't have this?`)
+let b = prompt(`how old am i?`)
+if (b == 12) alert(`depends at what time you see this tho`)
+if (b == 13) alert(`depends`)
+if (b <12 ) alert(` im not that young :(`)
+    alert(`anywais my gramar is probally bad because i don't have autocorecter, goodbye`)     
+alert(`p.s. the button does somthing now:) `)
+ 
+function displayText() {
+    var text = document.getElementById("textField");
+    text.style.display = "block";
+  }
